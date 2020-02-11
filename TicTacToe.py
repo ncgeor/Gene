0a1c977fceaab7345fec36473f433dfb31e96b17
Noah George <ncgeor@noreply.github.com>
2020-02-11 17:21:12 -0500

    Set Default TicTacToe
    The default game file. Realistically, I shouldn't have written anything in the player class but we should really only need those three things.
    
    When implimenting Gene, we should try to create an interface for players so we cab separate human and a.i. but I wasn't sure how to deal with that in python

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,1 +1,32 @@
-print('hello world! I am Gene')
+class Game:
+
+    def __init__(self):
+        pass
+
+    def turn(self, board, plyr):
+        # runs a turn for a player
+        pass
+
+    def hasWinner(self):
+        # Checks for and returns the winner
+        pass
+
+    class Player:
+
+        def __init__(self, n):
+            self.setChar(n)
+
+        def setChar(self, n):
+            self.char = n
+
+        def getChar(self):
+            return self.char
+
+
+    class Board:
+
+        def __str__(self):
+            # Needed for outputting the Board
+            # Front end developer's work here!
+            """ Must return a string """
+            pass
