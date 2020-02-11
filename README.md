2c052e1c9c3f050f9d66ebea68b2ddbe966cf89c
Noah George <ncgeor@noreply.github.com>
2020-02-11 18:10:00 -0500

    Unfinished Game
    Here is my unfinished version of the game. I'm just calling for the night and I'll finish it tommorrow

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ A machine educable noughts and crosses engine
 Gene plays TocTacToe until he either wins or ties.
 Our version of the bot checks percentage values of how likely
 it is for each move to win and will select a choice semi-randomly
-in order to promote break-throughs.  b
+in order to promote break-throughs.
--- TicTacToe.py
+++ TicTacToe.py
@@ -5,11 +5,19 @@ class Game:
 
     def turn(self, board, plyr):
         # runs a turn for a player
-        pass
+        print(board)
+        targetSpot = input("You would like to play in spot: ")
 
     def hasWinner(self):
         # Checks for and returns the winner
         pass
+
+    def playSpot(self, n, board):
+        # n is an int where the player would like to play
+        # check to see if it's free and place it
+        # returns True if placed, False if it cannot
+        pass
+
 
 class Player:
 
@@ -20,13 +28,48 @@ class Player:
         self.char = n
 
     def getChar(self):
-        return self.char
+        out = self.char
+        return out
+
+    def __str__(self):
+        # Will output this return if you try to print the class
+        return str.getChar()
 
 
 class Board:
+
+    def __init__(self):
+        self.board = [[None]*3]*3
+        self.setBlankBoard()
+
+    def setBlankBoard(self):
+        # Set every tile to None except for the center (which is Gene)
+        default = [[None]*3]*3
+        default[0][0] = 'O'
+        self.board = default
+
+    def getBoard(self):
+        out = self.board
+        return out
 
     def __str__(self):
         # Needed for outputting the Board
         # Front end developer's work here!
         """ Must return a string """
-        pass
+
+        out = ''
+
+        for i in range(3):
+            for j in range(3):
+                if(self.board[i][j] == None):
+                    out += 'X'
+                else:
+                    out += str(self.board[i][j])
+                if(j != 2):
+                    out += '|'
+            out += '\n-----\n'
+        return out
+
+if __name__ == '__main__':
+    j = Board()
+    print(j)
